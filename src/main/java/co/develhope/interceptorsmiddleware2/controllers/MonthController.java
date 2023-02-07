a01db6f57fe816ef2e6c405c131a59ed3a404eb0
Kram037 <defenzomarco@gmail.com>
2023-02-07 15:40:14 +0100

    Fix: fixed month related classes

--- src/main/java/co/develhope/interceptorsmiddleware2/controllers/MonthController.java
+++ src/main/java/co/develhope/interceptorsmiddleware2/controllers/MonthController.java
@@ -1,21 +1,14 @@
 package co.develhope.interceptorsmiddleware2.controllers;
 
 import co.develhope.interceptorsmiddleware2.entities.Month;
-import co.develhope.interceptorsmiddleware2.interceptors.MonthInterceptor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/months")
 public class MonthController {
 
-    @GetMapping
-    public Month getMonth(@RequestHeader(value = "monthNumber", required = true) Integer monthNumber) throws Exception {
-        if (monthNumber == null) {
-            throw new Exception("monthNumber header is missing");
-        }
-        return MonthInterceptor.getMonthByNumber(monthNumber);
+    @GetMapping("")
+    public Month getMonth(@RequestAttribute("month") Month month) {
+        return month;
     }
 }
